d5593aa7348b493c5fcf04b188d44d16c74dec5f
sharper <hi@sammyharper.com>
2025-07-01 22:24:45 -0500

    feat: target android sdk to 35
    Bump compileSdk and targetSdk versions from 34 to 35 in app module to support the latest Android platform features and requirements.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.terrabyte.byteosnotes"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.terrabyte.byteosnotes"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 3
         versionName = "1.2"
 
